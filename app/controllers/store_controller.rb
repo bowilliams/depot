11b04d03044b0efd3557c77489f8e6c8718e2d50
Bo Williams <bowilliams@gmail.com>
2011-09-30 20:40:52 -0700

    better cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+    @count = session[:counter]
     @products = Product.order(:title)
   end
 
